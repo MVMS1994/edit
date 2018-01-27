6adf5ef11eb8d4947b2f481000e3cd5d94767493
M Veera Manohara Subbbiah <mvm.subbiah@gmail.com>
2018-01-28 03:51:49 +0530

    temp server

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-var app = require('./output/Main/index.js');
+var app     = require('./output/Main/index.js');
+var express = require('express');
+var server  = express();
+var port    = 8081;
+
+server.use("/", function(req, res) {
+  res.send("UP");
+});
+
+server.listen(port, function() {
+  console.log("server started at " + port)
+})
 
 app.main();
